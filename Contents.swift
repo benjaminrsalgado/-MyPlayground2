697189f61b12fbcf0f8a59c719e20ec616d837b3
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-08-27 17:47:28 -0600

    i did an excersices

--- Contents.swift
+++ Contents.swift
@@ -1,13 +1,9 @@
 import Foundation
 
-func numeros(num1: Double, num2: Double, num3: Double) -> Double {
-    if num1 > num2 &&  num1 > num3 {
-        return num1
-    }else if num2 > num1 && num2 > num3{
-        return num2
+func pares (numero: Int)-> Int{
+    if numero % 2 == 0 {
+        return(numero)
     }else{
-        return num3
+        return(numero)
     }
 }
-
-numeros(num1: 20, num2: 50, num3: 5)
