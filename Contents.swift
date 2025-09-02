cad6f8689e3ef007a7c7defc9c03d820be9d0bc4
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-09-02 15:43:05 -0600

    i practice

--- Contents.swift
+++ Contents.swift
@@ -1,12 +1,7 @@
 import Foundation
 
-func Doblar(numero: String) -> String{
-    let convertir = Int(numero)!
-    
-    var multiplicar = convertir * 2
-    
-    var convertir2 = String(multiplicar)
-    return convertir2
-}
+let edad: Int = 22
+let convertir = String(edad)
+print(edad)
+print(convertir)
 
-Doblar(numero: "22")
