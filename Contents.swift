28194f4342b82913a0c02b0b4b6b8ced24b1ae92
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-09-02 14:45:35 -0600

    i practice greter than less than

--- Contents.swift
+++ Contents.swift
@@ -1,9 +1,17 @@
 import Foundation
 
-func pares (numero: Int)-> Int{
-    if numero % 2 == 0 {
-        return(numero)
-    }else{
-        return(numero)
+func edades(edad1: Int, edad2: Int)-> String{
+    if edad1 == edad2{
+        return("Tienen la misma edad")
+    }else if edad1 > edad2{
+        return("La primera persona es mayor")
+    }else if edad1 < edad2{
+        return("La segunda persona es mayor")
+    }else {
+        return "No se pudo comparar"
     }
 }
+
+edades(edad1: 20, edad2: 20) // "Tienen la misma edad"
+edades(edad1: 25, edad2: 18) // "La primera persona es mayor"
+edades(edad1: 15, edad2: 30) // "La segunda persona es mayor"
